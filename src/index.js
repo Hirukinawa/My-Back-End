08476402dcda4c748d33bdb5567f14b70ce524b2
João Pedro <joaopedrocode0@gmail.com>
2024-05-18 15:31:48 -0300

    v4

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 
-app.use(cors());
+app.use(cors({origin: 'http://localhost:5173'}));
 
 app.get('/', (req, res) => {
     res.send("Hello World!")
